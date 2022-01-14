5baaa7b0e57a380c517877077b4e4dae51c4a7f3
Naodk <D0218K70nao@gmail.com>
2022-01-14 06:33:22 +0000

    課題２をていしゅつ

--- kadai-02/index.js
+++ kadai-02/index.js
@@ -1,12 +1,17 @@
-const firstName　= "Nao"
-const familyName = "Oki"
-console.log(firstName + familyName)
+const firstName = "nao"
+const familyName = "oki"
+console.log(familyName + ", " + firstName)
 
-const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 
-console.log(sum) 
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
 
 const dog = false
 const cat = true
 console.log(dog || cat) //犬または猫を飼っているかどうか
 console.log(dog && cat) //犬と猫の両方を飼っている
-console.log(!dog && !cat) //犬も猫も飼っていない
+console.log(!dog && !cat) //犬も猫もかっていない
+console.log(!(dog || cat))
+
+//おまけ
+console.log(cat && !dog)//猫を飼っているが犬は飼っていない
+console.log(dog && !cat)//犬を飼っているが猫は飼っていない
