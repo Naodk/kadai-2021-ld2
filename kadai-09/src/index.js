f5760c1e951c2f7ad86f443420f410edaf74ce54
Naodk <D0218K70nao@gmail.com>
2022-01-15 02:26:02 +0000

    課題９を追加

--- kadai-09/src/index.js
+++ kadai-09/src/index.js
@@ -1,9 +1,6 @@
+
 const moduleA = require("./module-a.js")
 const moduleB = require("./module-b.js")
 
-
 moduleA()
-console.log(moduleB)
-
-const content = fs.readFileSync("./kadai-09/sample/hello.text").toString("utf-8")
-console.log(content)
+moduleB()
